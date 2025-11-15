a2c3fc14962909f62e5eeb357cb2fca4879d4691
Ahmed-malik <159722798+ahmed22138@users.noreply.github.com>
2025-11-15 21:00:39 +0500

    Update page.tsx

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -22,7 +22,7 @@ export default function ChatPage() {
     setMessage("");
     setLoading(true);
 
-    const res = await fetch("http://127.0.0.1:8000/chat", {
+    const res = await fetch("https://vercel.com/ahmed-maliks-projects-3b2073df/backend-portfolio", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message }),
